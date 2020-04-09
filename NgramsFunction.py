5efa6f9695a88c0ec7bf83614bcd4a72212c68c6
Siabell <valesiabatto@gmail.com>
2020-04-08 19:03:39 -0500

    n-grams function bordes

--- NgramsFunction.py
+++ NgramsFunction.py
@@ -1,16 +1,26 @@
 
 def calculateNGrams(text, n):
-    """ calculates the ngrams of a text of n items, and returns the most
-    frequent n-gram in a complexity of O(n+n)"""
+    """ calculates the n-grams of a text of "n" items, and returns a list
+    of the n-grams, in a complexity of O(n)"""
     nGrams=[]
+    if (len(text)<n or n<1 ):
+        raise Exception('The "n" value is not compatible for the n-grams')
+    elif (len(text)==0):
+        raise Exception('The "text" value is not compatible for the n-grams')
     for i in range(len(text)-(n-1)):
         nGrams.append(text[i:i+n])
+    return nGrams
+
+def mostFrequentNGram(text, n):
+    """ calculates the n-grams of a text of "n" items, and returns the most
+    frequent n-gram, in a complexity of O(n+n)"""
+    nGrams = calculateNGrams(text,n)
     mostFrequent = mostFrequencyItem(nGrams)
     return mostFrequent
 
 
 def mostFrequencyItem(itemList):
-    """receives a list of items and returns the most frequent item in a
+    """receives a list of items and returns the most frequent item, in a
     complexity of O(n) """
     nGramsDict={}
     mostFrequentValue = 0
